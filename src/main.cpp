33a802dce5d5ebeed34fbfdf3320bf411a61742c
Pietro Bonaldo Gregori <pietro@Pietros-Air.lan>
2022-07-28 14:04:46 +0200

    use VertexArray

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,46 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 
-bool debug = false;
+bool debug = true;
+float line_length = 1000;
 
-float to_rad(float deg);
-float to_deg(float rad);
-
-sf::Vector2f intersection(sf::Vector2f line1[], sf::Vector2f line2[]);
-bool is_between(sf::Vector2f line[], sf::Vector2f point);
+sf::Vector2f intersection(sf::Vector2f line1_point1, sf::Vector2f line1_point2, sf::Vector2f line2_point1, sf::Vector2f line2_point2);
+bool is_between(sf::Vector2f line_point1, sf::Vector2f line_point2, sf::Vector2f point);
 
 sf::CircleShape mark_point(sf::Vector2f point, sf::Color color=sf::Color::Blue);
-sf::VertexArray draw_line(sf::Vector2f points[], int extend=0);
+sf::VertexArray mark_line(sf::Vector2f point1, sf::Vector2f point2, int extend=0);
 
 int main(int argc, char *argv[])
 {
+        // Arguments
         int frequency = 6;
         if (argc > 1 && strtol(argv[1], NULL, 10) > 0) frequency = strtol(argv[1], NULL, 10);
         int lines_number = (360/frequency);
+        
 
+        // Window Setup
         sf::ContextSettings settings;
             settings.antialiasingLevel = 4;
         sf::RenderWindow window(sf::VideoMode(1400, 950), "Raycasting", sf::Style::Default, settings);
         window.setFramerateLimit(60);
 
-        sf::RectangleShape line(sf::Vector2f(1, 300));
-        sf::RectangleShape wall(sf::Vector2f(25, 300));
-            wall.setPosition(850, 300);
-            wall.setRotation(60);
-            wall.setFillColor(sf::Color::Red);
-        sf::Vector2f wall_points[wall.getPointCount()][wall.getPointCount() / 2];
-        for (int i = 0; i < wall.getPointCount(); i++){
-            wall_points[i][0] = wall.getTransform().transformPoint(wall.getPoint(i));
-            wall_points[i][1] = wall.getTransform().transformPoint(wall.getPoint(i+1));
+        // Shape, Hitbox, Line Setup
+        sf::VertexArray line(sf::Lines, 2);
+
+        sf::RectangleShape shape(sf::Vector2f(80, 400));
+            shape.setPosition(500, 300);
+            shape.setRotation(60);
+            shape.setFillColor(sf::Color::Transparent);
+            shape.setOutlineColor(sf::Color::Red);
+            shape.setOutlineThickness(1);
+        int shape_sides = shape.getPointCount();
+
+        sf::VertexArray obstacle(sf::Lines, shape_sides+1);
+        for (int i = 0; i < shape_sides; i++){
+            obstacle[i].position = shape.getTransform().transformPoint(shape.getPoint(i));
+            obstacle[i].color = sf::Color::Yellow;
         }
+        obstacle[shape_sides].position = obstacle[0].position;
 
         while (window.isOpen()){
 
@@ -52,39 +60,40 @@ int main(int argc, char *argv[])
                 float mouse_x = sf::Mouse::getPosition(window).x;
                 float mouse_y = sf::Mouse::getPosition(window).y;
 
-                line.setPosition(mouse_x, mouse_y);
                 for(int i = 0; i < lines_number; i++){
-                    float rotation = i*frequency;
-                    line.setRotation(rotation);
-                    line.setSize(sf::Vector2f(line.getSize().x, 500));
+                    float rotation = (i*frequency) * M_PI / 180; // rotation in radians
 
-                    sf::Vector2f line_points[line.getPointCount() / 2];
-                        line_points[0] = line.getTransform().transformPoint(line.getPoint(0));
-                        line_points[1] = line.getTransform().transformPoint(line.getPoint(3));
+                    // Set line endpoints
+                    line[0].position = sf::Vector2f(mouse_x, mouse_y);
+                    float radius = line_length;
+                    float line_x = mouse_x - radius * sin(rotation);
+                    float line_y = mouse_y + radius * cos(rotation);
+                    line[1].position = sf::Vector2f(line_x, line_y);
 
-                    line.setFillColor(sf::Color::White);
-                    for(int i = 0; i < wall.getPointCount(); i++){
-                        if(debug) window.draw(draw_line(wall_points[i], 1000));
+                    // Wall collision
+                    for(int i = 0; i < shape_sides; i++){
+                        if(debug) window.draw(mark_line(obstacle[i].position, obstacle[i+1].position, 1000));
 
-                        sf::Vector2f intersection_point = intersection(line_points, wall_points[i]);
+                        sf::Vector2f intersection_point = intersection(line[0].position, line[1].position, obstacle[i].position, obstacle[i+1].position);
                         // this is unreadable but its just pythagoras
-                        float distance = sqrt(pow(line_points[0].x - intersection_point.x, 2) + pow(line_points[0].y - intersection_point.y, 2));
-                        if(distance <= line.getSize().y){
-                            if( is_between(line_points, intersection_point) &&
-                                is_between(wall_points[i], intersection_point)) {
-                                    line.setSize(sf::Vector2f(line.getSize().x, distance));
+                        float distance = sqrt(pow(line[0].position.x - intersection_point.x, 2) + pow(line[0].position.y - intersection_point.y, 2));
+                        if(distance <= line_length){
+                            if( is_between(line[0].position, line[1].position, intersection_point) &&
+                                is_between(obstacle[i].position, obstacle[i+1].position, intersection_point)) {
+                                    if(debug) window.draw(mark_point(intersection_point));
+                                    line[1].position = intersection_point;
                             }
                         }
 
-                        if(debug) window.draw(mark_point(intersection_point));
+                        if(debug) window.draw(mark_point(intersection_point, sf::Color(100, 100, 100, 100)));
                     }
 
+                    // Render
                     window.draw(line);
-                    window.draw(wall);
+                    window.draw(shape);
                     if(debug){
-                        for(int i = 0; i < wall.getPointCount(); i++){
-                            window.draw(mark_point(wall_points[i][0], sf::Color::Green));
-                            window.draw(mark_point(wall_points[i][1], sf::Color::Green));
+                        for(int i = 0; i < shape_sides; i++){
+                            window.draw(mark_point(obstacle[i].position, sf::Color::Green));
                         }
                     }
                 }
@@ -95,31 +104,24 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-float to_rad(float deg){
-    return deg * M_PI / 180;
-}
-
-float to_deg(float rad){
-    return rad * 180 / M_PI;
-}
-
-bool is_between(sf::Vector2f line[], sf::Vector2f point){
-    if( point.x <= std::max(line[0].x, line[1].x) &&
-        point.x >= std::min(line[0].x, line[1].x) &&
-        point.y <= std::max(line[0].y, line[1].y) &&
-        point.y >= std::min(line[0].y, line[1].y)
+bool is_between(sf::Vector2f line_point1, sf::Vector2f line_point2, sf::Vector2f point){
+    if( point.x <= std::max(line_point1.x, line_point2.x) &&
+        point.x >= std::min(line_point1.x, line_point2.x) &&
+        point.y <= std::max(line_point1.y, line_point2.y) &&
+        point.y >= std::min(line_point1.y, line_point2.y)
         ) return true;
     return false;
 }
 
-sf::Vector2f intersection(sf::Vector2f line1[], sf::Vector2f line2[]) {
-   double a = line1[1].y - line1[0].y;
-   double b = line1[0].x - line1[1].x;
-   double c = a*(line1[0].x) + b*(line1[0].y);
+sf::Vector2f intersection(sf::Vector2f line1_point1, sf::Vector2f line1_point2, 
+                          sf::Vector2f line2_point1, sf::Vector2f line2_point2) {
+   double a = line1_point2.y - line1_point1.y;
+   double b = line1_point1.x - line1_point2.x;
+   double c = a*(line1_point1.x) + b*(line1_point1.y);
 
-   double a1 = line2[1].y - line2[0].y;
-   double b1 = line2[0].x - line2[1].x;
-   double c1 = a1*(line2[0].x)+ b1*(line2[0].y);
+   double a1 = line2_point2.y - line2_point1.y;
+   double b1 = line2_point1.x - line2_point2.x;
+   double c1 = a1*(line2_point1.x)+ b1*(line2_point1.y);
    double det = a*b1 - a1*b;
    if (det == 0) {
       return sf::Vector2f(9999999, 9999999);
@@ -138,14 +140,14 @@ sf::CircleShape mark_point(sf::Vector2f point, sf::Color color){
     return marker;
 }
 
-sf::VertexArray draw_line(sf::Vector2f points[], int extend){
+sf::VertexArray mark_line(sf::Vector2f point1, sf::Vector2f point2, int extend){
     sf::VertexArray line(sf::Lines, 2);
-    float gradient = (points[1].y - points[0].y) / (points[1].x - points[0].x);
+    float gradient = (point2.y - point1.y) / (point2.x - point1.x);
     
-    float x1 = points[0].x - extend;
-    float y1 = points[0].y - (gradient*extend);
-    float x2 = points[1].x + extend;
-    float y2 = points[1].y + (gradient*extend);
+    float x1 = point1.x - extend;
+    float y1 = point1.y - (gradient*extend);
+    float x2 = point2.x + extend;
+    float y2 = point2.y + (gradient*extend);
     line[0].position = sf::Vector2f(x1, y1);
     line[1].position = sf::Vector2f(x2, y2);
 
